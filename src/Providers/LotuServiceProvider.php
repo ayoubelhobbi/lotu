a23bc8ae2963116b848b72d9fc0cfe21f5d677ba
Ayoub <ayoubelhobbi@gmail.com>
2018-06-28 12:18:51 +0200

    Footer ändern

--- src/Providers/LotuServiceProvider.php
+++ src/Providers/LotuServiceProvider.php
@@ -14,4 +14,17 @@ class LotuServiceProvider extends ServiceProvider
 	{
 
 	}
+
+	/**
+ * Boot a template for the footer that will be displayed in the template plugin instead of the original footer.
+ */
+public function boot(Twig $twig, Dispatcher $eventDispatcher)
+	{
+			$eventDispatcher->listen('IO.init.templates', function(Partial $partial)
+			{
+				 $partial->set('footer', 'Lotu::content.ThemeFooter');
+			}, 0);
+			return false;
+	}
+
 }
