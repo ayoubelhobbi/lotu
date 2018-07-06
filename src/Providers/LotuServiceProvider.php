d03f20609c312185503852409cba71026ee5cd5d
Ayoub <ayoubelhobbi@gmail.com>
2018-07-06 10:14:38 +0200

    item überschreiben

--- src/Providers/LotuServiceProvider.php
+++ src/Providers/LotuServiceProvider.php
@@ -6,9 +6,12 @@ use IO\Extensions\Functions\Partial;
 use Plenty\Plugin\Events\Dispatcher;
 use Plenty\Plugin\ServiceProvider;
 use Plenty\Plugin\Templates\Twig;
+use IO\Helper\TemplateContainer;
 
 class LotuServiceProvider extends ServiceProvider
 {
+
+	const PRIORITY = 0;
 
 	/**
 	 * Register the service provider.
@@ -28,5 +31,14 @@ class LotuServiceProvider extends ServiceProvider
 					$partial->set('footer', 'Lotu::ThemeFooter');
 			 }, 0);
 			 return false;
+
+			 $eventDispatcher->listen('IO.tpl.item', function (TemplateContainer $container)
+        {
+            $container->setTemplate('Lotu::Item.SingleItem');
+            return false;
+        }, self::PRIORITY);
 	 }
+
+
+
 }
